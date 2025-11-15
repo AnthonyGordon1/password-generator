28cfd386b2bb111e6c5f66bf15285a90a12cd6f0
Anthony Gordon <anthonygordonunt@gmail.com>
2025-11-15 09:13:39 -0600

    Refactored Code

--- password-generator.py
+++ password-generator.py
@@ -1,11 +1,26 @@
 import string
 import secrets
 
+# Use secrets module for cryptographic security
 def generate_password(length: int = 15):
+    #Use safe punctuation
     safe_punctuation = "!@#$%^&*"
     chars = string.ascii_letters + string.digits + safe_punctuation
-    password = ''.join(secrets.choice(chars) for i in range(length))
-    return password
+
+    #Use at least one of the char types
+    password = [
+        secrets.choice(string.ascii_lowercase),
+        secrets.choice(string.ascii_uppercase),
+        secrets.choice(string.digits),
+        secrets.choice(safe_punctuation),
+    ]
+    
+    #Fill in the rest randomly
+    password += [secrets.choice(chars) for i in range(length - 4)]
+
+    #Shuffle around characters for more randomization
+    secrets.SystemRandom().shuffle(password)
+    return ''.join(password)
 
 password = generate_password()
 print("Generated password: " + password)
